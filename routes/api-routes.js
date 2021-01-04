77e1f4ba57aa264c6c8b81a62e3dced8c5e82963
Hayes11 <bhayes81@gmail.com>
2021-01-03 22:37:30 -0500

    added put route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
+    db.Workout.find()
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
@@ -14,6 +14,16 @@ router.get("/api/workouts", (req, res) => {
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
         //.then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { workouts: _id } }, { new: true }))
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
