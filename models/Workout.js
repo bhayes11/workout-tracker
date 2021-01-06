78d10dee025128b6168dc702120300f9be98a432
Hayes11 <bhayes81@gmail.com>
2021-01-06 18:35:13 -0500

    added body to workouit model and update api put route

--- models/Workout.js
+++ models/Workout.js
@@ -2,15 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: Date,
+  day:{ type:Date, default:() => Date(), },
   exercises: [
     {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter an exercise type',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter exercise name',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter exercise duration in minutes',
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
     }
   ]
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -44,15 +44,30 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
-router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        .then((updatedWorkout) => {
-            res.json(updatedWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// router.put("/api/workouts/:id", (req, res) => {
+//     db.Workout.findByIdAndUpdate(req.params.id, req.body, {new: true})
+//         .then((updatedWorkout) => {
+//             res.json(updatedWorkout);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      // "runValidators" will ensure new exercises meet our schema requirements
+      { new: true, runValidators: true }
+    )
+      .then((updateWorkout) => {
+        res.json(updateWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find().limit(7)
