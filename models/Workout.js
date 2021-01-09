7e9371bb3fc70166e9ceab5225e0cf91d81b2150
Hayes11 <bhayes81@gmail.com>
2021-01-08 21:11:05 -0500

    added duration function to workout js file

--- models/Workout.js
+++ models/Workout.js
@@ -2,35 +2,44 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day:{ type:Date, default:() => Date(), },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: 'Enter an exercise type',
+  day: { type: Date, default: () => Date(), },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: 'Enter an exercise type',
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: 'Enter exercise name',
+        },
+        duration: {
+          type: Number,
+          required: 'Enter exercise duration in minutes',
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
       },
-      name: {
-        type: String,
-        trim: true,
-        required: 'Enter exercise name',
-      },
-      duration: {
-        type: Number,
-        required: 'Enter exercise duration in minutes',
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-    }
-  ]
+    ],
+  },
+  { toJSON: { virtuals: true}}
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((totalDuration, exercise) => {
+    return totalDuration + exercise.duration;
+  }, 0);
 });
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
